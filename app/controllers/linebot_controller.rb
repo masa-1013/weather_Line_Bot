4a201d736ff7d25dd0cf9c1e1fe94d5519dc656c
masahiro <masahiro@masanoMacBook-Pro.local>
2019-11-17 23:34:34 +0900

    add openweather api

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,6 +1,9 @@
 class LinebotController < ApplicationController
-  skip_before_action :verify_authenticity_token
   require 'line/bot'
+  require 'json'
+  require 'open-uri'
+
+  skip_before_action :verify_authenticity_token
 
   def callback
     body = request.body.read
@@ -17,21 +20,31 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           message = {
             type: 'text',
-            text: event.message['text']
+            text: call_weather
           }
         end
       end
       client.reply_message(event['replyToken'], message)
     end
     head :ok
+
+    p call_weather
   end
 
   private
 
   def client
     @client ||= Line::Bot::Client.new { |config| 
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
     }
+  end
+
+  def call_weather
+    API_KEY = ENV['OPEN_WEATHER_API_KEY']
+    BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast'
+
+    response = open(BASE_URL + "?q=Osaka,jp&APPID=#{API_KEY}")
+    JSON.pretty_generate(JSON.parse(response.read))
   end
 end
